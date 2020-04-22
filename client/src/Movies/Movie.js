d1265693ef59d78071c8c0afd75f32ae4ceecb00
Elijah Atkins <elijahorama@gmail.com>
2020-04-22 11:58:31 -0600

    refactored code in movieCard

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard';
 import { useRouteMatch } from 'react-router-dom';
 
 const Movie = (props) => {
@@ -32,25 +33,10 @@ const Movie = (props) => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
+  const { id, title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      <MovieCard key={movie.id} movie={movie} />
       <div className="save-button">Save</div>
     </div>
   );
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MovieCard = props => {
-  return;
+  const movie = props.movie;
+
+  return (
+    <Link to={`/movies/${movie.id}`}>
+    <div className="movie-card">
+      <h2>{movie.title}</h2>
+      <div className="movie-director">
+        Director: <em>{movie.director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{movie.metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+
+      {movie.stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
+    </Link>
+  )
 };
 
 export default MovieCard;
